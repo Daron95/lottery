7189481b645172d14c0ee510304c35d9f4ea7047
Daron Kirakossian <daron.kirakossian@almasar-solutions.com>
2024-03-19 22:04:23 +0200

    commit16

--- filenew.java
+++ filenew.java
@@ -2,4 +2,7 @@ System.out.println("HEllo");
 System.out.println("HEllo1");
 System.out.println("HEllo2");
 System.out.println("HEllo3");
+System.out.println("Bye11"); 
+System.out.println("Bye12"); 
+System.out.println("Bye13"); 
 
